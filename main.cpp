02aac02a2581d084d77a7a85db1215966d39d856
max <max@mkoldaev.ru>
2021-03-30 01:28:05 +0300

    Бытие

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include <fstream>
 #include <string>
 #include "Bible1.h"
+#include <cstdlib>
 using namespace std;
 
-int main() {
-    //cout << rubible1_1;
-    //cout << "\n";
-    Bible1::view1_1();
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        int val = atoi(argv[1]);
+        if(val==1) Bible1::view1();
+        else if(val==2) Bible1::view2();
+        else if(val==3) Bible1::view3();
+    } else {
+        Bible1::view1();
+    }
 //    ifstream file("./ru/1/bible_ru_1_1.txt");
 //    if (file.is_open()) {
 //        string line;
