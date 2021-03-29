1826ca9fcb4e6d4a595ab677c0d0bf8628647ffe
max <max@mkoldaev.ru>
2021-03-30 00:53:32 +0300

    Бытие

---
+++ Bible1.h
@@ -0,0 +1,97 @@
+#include <map>
+#include <string>
+class Bible1
+{
+#define rubible1_1 "rectangle"
+    struct ru1	{ int val; const char *msg; };
+    struct ru2	{ int val; const char *msg; };
+    struct ru3	{ int val; const char *msg; };
+    struct ru4	{ int val; const char *msg; };
+    struct ru5	{ int val; const char *msg; };
+    struct ru6	{ int val; const char *msg; };
+    struct ru7	{ int val; const char *msg; };
+    struct ru8	{ int val; const char *msg; };
+    struct ru9	{ int val; const char *msg; };
+    struct ru10	{ int val; const char *msg; };
+    struct ru11	{ int val; const char *msg; };
+    struct ru12	{ int val; const char *msg; };
+    struct ru13	{ int val; const char *msg; };
+    struct ru14	{ int val; const char *msg; };
+    struct ru15	{ int val; const char *msg; };
+    struct ru16	{ int val; const char *msg; };
+    struct ru17	{ int val; const char *msg; };
+    struct ru18	{ int val; const char *msg; };
+    struct ru19	{ int val; const char *msg; };
+    struct ru20	{ int val; const char *msg; };
+    struct ru21	{ int val; const char *msg; };
+    struct ru22	{ int val; const char *msg; };
+    struct ru23	{ int val; const char *msg; };
+    struct ru24	{ int val; const char *msg; };
+    struct ru25	{ int val; const char *msg; };
+    struct ru26	{ int val; const char *msg; };
+    struct ru27	{ int val; const char *msg; };
+    struct ru28	{ int val; const char *msg; };
+    struct ru29	{ int val; const char *msg; };
+    struct ru30	{ int val; const char *msg; };
+    struct ru31	{ int val; const char *msg; };
+    struct ru32	{ int val; const char *msg; };
+    struct ru33	{ int val; const char *msg; };
+    struct ru34	{ int val; const char *msg; };
+    struct ru35	{ int val; const char *msg; };
+    struct ru36	{ int val; const char *msg; };
+    struct ru37	{ int val; const char *msg; };
+    struct ru38	{ int val; const char *msg; };
+    struct ru39	{ int val; const char *msg; };
+    struct ru40	{ int val; const char *msg; };
+    struct ru41	{ int val; const char *msg; };
+    struct ru42	{ int val; const char *msg; };
+    struct ru43	{ int val; const char *msg; };
+    struct ru44	{ int val; const char *msg; };
+    struct ru45	{ int val; const char *msg; };
+    struct ru46	{ int val; const char *msg; };
+    struct ru47	{ int val; const char *msg; };
+    struct ru48	{ int val; const char *msg; };
+    struct ru49	{ int val; const char *msg; };
+    struct ru50	{ int val; const char *msg; };
+public:
+static void view1_1() {
+struct ru1 poems[] = {
+        {1,"В начале сотворил Бог небо и землю."},
+        {2,"Земля же была безвидна и пуста, и тьма над бездною, и Дух Божий носился над водою."},
+        {3,"И сказал Бог: да будет свет. И стал свет."},
+        {4,"И увидел Бог свет, что он хорош, и отделил Бог свет от тьмы."},
+        {5,"И назвал Бог свет днем, а тьму ночью. И был вечер, и было утро: день один."},
+        {6,"И сказал Бог: да будет твердь посреди воды, и да отделяет она воду от воды."},
+        {7,"И создал Бог твердь, и отделил воду, которая под твердью, от воды, которая над твердью. И стало так."},
+        {8,"И назвал Бог твердь небом. И был вечер, и было утро: день второй."},
+        {9,"И сказал Бог: да соберется вода, которая под небом, в одно место, и да явится суша. И стало так."},
+        {10,"И назвал Бог сушу землею, а собрание вод назвал морями. И увидел Бог, что это хорошо."},
+        {11,"И сказал Бог: да произрастит земля зелень, траву, сеющую семя, дерево плодовитое, приносящее по роду своему плод, в котором семя его на земле. И стало так."},
+        {12,"И произвела земля зелень, траву, сеющую семя по роду ее, и дерево, приносящее плод, в котором семя его по роду его. И увидел Бог, что это хорошо."},
+        {13,"И был вечер, и было утро: день третий."},
+        {14,"И сказал Бог: да будут светила на тверди небесной для отделения дня от ночи, и для знамений, и времен, и дней, и годов;"},
+        {15,"и да будут они светильниками на тверди небесной, чтобы светить на землю. И стало так."},
+        {16,"И создал Бог два светила великие: светило большее, для управления днем, и светило меньшее, для управления ночью, и звезды;"},
+        {17,"и поставил их Бог на тверди небесной, чтобы светить на землю,"},
+        {18,"и управлять днем и ночью, и отделять свет от тьмы. И увидел Бог, что это хорошо."},
+        {19,"И был вечер, и было утро: день четвёртый."},
+        {20,"И сказал Бог: да произведет вода пресмыкающихся, душу живую; и птицы да полетят над землею, по тверди небесной."},
+        {21,"И сотворил Бог рыб больших и всякую душу животных пресмыкающихся, которых произвела вода, по роду их, и всякую птицу пернатую по роду ее. И увидел Бог, что это хорошо."},
+        {22,"И благословил их Бог, говоря: плодитесь и размножайтесь, и наполняйте воды в морях, и птицы да размножаются на земле."},
+        {23,"И был вечер, и было утро: день пятый."},
+        {24,"И сказал Бог: да произведет земля душу живую по роду ее, скотов, и гадов, и зверей земных по роду их. И стало так."},
+        {25,"И создал Бог зверей земных по роду их, и скот по роду его, и всех гадов земных по роду их. И увидел Бог, что это хорошо."},
+        {26,"И сказал Бог: сотворим человека по образу Нашему и по подобию Нашему, и да владычествуют они над рыбами морскими, и над птицами небесными, и над скотом, и над всею землею, и над всеми гадами, пресмыкающимися по земле."},
+        {27,"И сотворил Бог человека по образу Своему, по образу Божию сотворил его; мужчину и женщину сотворил их."},
+        {28,"И благословил их Бог, и сказал им Бог: плодитесь и размножайтесь, и наполняйте землю, и обладайте ею, и владычествуйте над рыбами морскими, и над птицами небесными, и над всяким животным, пресмыкающимся по земле."},
+        {29,"И сказал Бог: вот, Я дал вам всякую траву, сеющую семя, какая есть на всей земле, и всякое дерево, у которого плод древесный, сеющий семя; - вам сие будет в пищу;"},
+        {30,"А всем зверям земным, и всем птицам небесным, и всякому пресмыкающемуся по земле, в котором душа живая, дал Я всю зелень травную в пищу. И стало так."},
+        {31,"И увидел Бог все, что Он создал, и вот, хорошо весьма. И был вечер, и было утро: день шестой."},
+};
+size_t npoems = sizeof poems / sizeof poems[0];
+size_t i;
+for (i=0; i < npoems; ++i) {
+printf("%d. %s\n", poems[i].val, poems[i].msg);
+}
+}
+};
--- main.cpp
+++ main.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <io.h>
-#include <fcntl.h>
-#include <sstream>
-#include <Windows.h>
-#include <cstdio>
-
+#include "Bible1.h"
 using namespace std;
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    setvbuf(stdout, nullptr, _IOFBF, 1000);
-    ifstream file("../ru/1/bible_ru_1_1.txt");
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            // using printf() in all tests for consistency
-            printf("%s", line.c_str());
-            cout << "\n";
-        }
-        file.close();
-    }
+    //cout << rubible1_1;
+    //cout << "\n";
+    Bible1::view1_1();
+//    ifstream file("./ru/1/bible_ru_1_1.txt");
+//    if (file.is_open()) {
+//        string line;
+//        while (getline(file, line)) {
+//            cout << line+"\n";
+//        }
+//        file.close();
+//    }
     return 0;
 }
